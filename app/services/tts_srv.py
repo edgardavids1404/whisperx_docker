6d964f2349053913291220cf28f4e36eb93ff94f
Edgar Davids <edgardavids1404@gmail.com>
2025-07-15 07:54:38 +0330

    tts bugs fixed + actions cache added

--- app/services/tts_srv.py
+++ app/services/tts_srv.py
@@ -6,7 +6,8 @@ from piper import PiperVoice
 def text_to_speech(text: str) -> str:
     config = get_config() 
     dst = pathlib.Path(config["tmp_dir"]) / f"{uuid.uuid4()}.wav"
-    voice = Pipervoice.load(config["piper"]["voice_path"])
-    with wave.open(dst, "wb") as wav_file:
-        voice.synthesize_wav(text.encode(), wav_file)
+    voice = PiperVoice.load(config["piper"]["voice_path"])
+    with open(dst, "wb") as f:
+        with wave.open(f, "wb") as wav_file:
+            voice.synthesize_wav(text, wav_file)
     return dst.as_posix()
